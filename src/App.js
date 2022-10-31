2e4fa4701c0ae5e2f2e111896483ddf90e728d96
Ayse <aysesadioglu@protonmail.com>
2022-10-31 11:47:29 +0300

    recent updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+import {useState} from 'react'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [feedback,setFeedBack]=useState(
+
+   
+      {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      },
+      {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      },
+      {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      },
+    
+  )
+ return ( 
+ 
+ <>
+  <Header />
+  <div className="container">
+   <FeedbackItem/>
+  </div>
+</>)
+   
+  
+}
 export default App;
+ 
