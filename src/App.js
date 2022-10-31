b8c6a098679773f9ab01c1de6156edb14d8a4026
Ayse <aysesadioglu@protonmail.com>
2022-10-31 16:30:16 +0300

    can't locate the unnecessary comma

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+
+
 
 function App() {
   const [feedback, setFeedBack] = useState(FeedbackData);
@@ -18,6 +21,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm/>
         <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,29 @@
+import Card from "./shared/Card"
+import { useState } from "react"
+
+
+function FeedbackForm() {
+    const [text,setText]=useState('')
+
+    const handleTextChange =(e)=>{
+        setText(e.target.value)
+
+    }
+
+    return <Card>
+        <form >
+            <h2>How yould you rate your service with us?</h2>
+            {/*@todo - rating select component */}
+            <div className="input-group">
+
+                <input onChange={handleTextChange} type="text" placeholder="Write a review" 
+                value={text}/>
+                <button type="submit">Send</button>
+            </div>
+
+
+        </form>
+        </Card>
+    
+}
+export default FeedbackForm
