61eb4c2e880ac60e25581837f273384d6956b766
Ayse <aysesadioglu@protonmail.com>
2022-11-01 13:54:41 +0300

    animation added

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,36 +1,41 @@
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 
-
-function FeedbackList({feedback,handleDelete}) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
   }
+
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item}
-        handleDelete={handleDelete}
-        />
-      ))}
-   
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div key={item.id}
+          initial={{opacity:0}}
+          animate={{opacity:1}} 
+          exit={{opacity:0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
 
-
-FeedbackList.propTypes={
-feedback:PropTypes.arrayOf(
-  PropTypes.shape({
-    id:PropTypes.number.isRequired,
-    text:PropTypes.string.isRequired,
-    rating:PropTypes.number.isRequired,
-
-
-  })
-),
-
-}
-
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
